643d673c1a3429d6e0cf0cec8e5e047fadf127c0
adilhusain01 <husainadil202@gmail.com>
2024-02-18 23:33:11 +0530

    Add login route to userRoute.js

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, createUser, updateUser, deleteUser, getUserById } = require("../controllers/userController");
+const { getUsers, createUser, updateUser, deleteUser, getUserById, login } = require("../controllers/userController");
 
 router.get('/', getUsers);
+router.get('/:id', getUserById);
 router.post('/', createUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
-router.get('/:id', getUserById);
 
 module.exports = router;
